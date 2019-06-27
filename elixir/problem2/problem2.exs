b700893741b0f8ce08edff0085397e1ee2cbf1c1
Andre LaFleur <andre.lafleur@utah.edu>
2019-06-27 14:16:51 -0600

    Solve problem 2

---
+++ elixir/problem2/problem2.exs
@@ -0,0 +1,17 @@
+# I stole this module from http://stratus3d.com/blog/2018/08/18/fibonacci-algorithms-in-elixir/
+defmodule Fibonacci do
+	def fib(0), do: 0
+	def fib(1), do: 1
+	def fib(n), do: fib(0, 1, n - 2)
+
+	def fib(_, prv, -1), do:  prv
+	def fib(prvprv, prv, n) do
+		next = prv + prvprv
+		fib(prv, next, n - 1)
+	end
+end
+
+IO.inspect Enum.map(0..50, fn i -> Fibonacci.fib(i) end) |> 
+	Enum.filter(fn i -> i <= 4000000 end) |>
+	Enum.filter(fn i -> rem(i, 2) === 0 end) |>
+	Enum.sum

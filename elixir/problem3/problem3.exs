579d7ab2cb26bd6606a6e6ec2ed7236de52e4e02
Andre LaFleur <andre.lafleur@utah.edu>
2020-05-02 15:24:40 -0600

    Finish problem 3

--- elixir/problem3/problem3.exs
+++ elixir/problem3/problem3.exs
@@ -2,10 +2,23 @@ defmodule Primality do
 	def is_prime(2), do: true
 	def is_prime(3), do: true
 	def is_prime(x) do
-		range = 2..x
-		Enum.map(range, fn num -> rem(x, num) end)
-			|> Enum.count(fn num -> num === 0 end) === 1
+		limit = round(:math.sqrt(x))
+		range = 2..limit
+		remainders_of(range, x)
+			|> Enum.count(fn remainder -> remainder === 0 end) === 0
+	end
+
+	def largest_prime_factor_of(value) do
+		limit = round(:math.sqrt(value))
+		range = 2..limit
+		Enum.filter(range, fn x -> rem(value, x) === 0 end)
+			|> Enum.filter(fn x -> is_prime(x) end)
+			|> Enum.max
+	end
+
+	defp remainders_of(range, dividend) do
+		Enum.map(range, fn potential_factor -> rem(dividend, potential_factor) end)
 	end
 end
 
-IO.inspect Primality.is_prime(600)
+IO.inspect Primality.largest_prime_factor_of(600851475143)

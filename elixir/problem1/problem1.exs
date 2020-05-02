ae3579d4695a40c23338ab7a3c2ec8b25dbeaddf
Andre LaFleur <andre.lafleur@utah.edu>
2020-05-02 14:49:30 -0600

    Cleanup some code

--- elixir/problem1/problem1.exs
+++ elixir/problem1/problem1.exs
@@ -1,3 +1,11 @@
+defmodule Multiples do
+	def three_or_five(x) do
+		rem(x, 3) === 0 or rem(x, 5) === 0
+	end
+end
+
 range = 1..(1000 - 1)
-result = Enum.sum(Enum.filter(range, fn x -> rem(x, 3) === 0 or rem(x, 5) === 0 end))
-IO.inspect result
+is_multiple = fn x -> Multiples.three_or_five(x) end
+Enum.filter(range, is_multiple)
+	|> Enum.sum
+	|> IO.inspect
--- elixir/problem2/problem2.exs
+++ elixir/problem2/problem2.exs
@@ -11,7 +11,7 @@ defmodule Fibonacci do
 	end
 end
 
-IO.inspect Enum.map(0..50, fn i -> Fibonacci.fib(i) end) |> 
-	Enum.filter(fn i -> i <= 4000000 end) |>
-	Enum.filter(fn i -> rem(i, 2) === 0 end) |>
-	Enum.sum
+IO.inspect Enum.map(0..50, fn i -> Fibonacci.fib(i) end)
+	|> Enum.filter(fn i -> i <= 4000000 end)
+	|> Enum.filter(fn i -> rem(i, 2) === 0 end)
+	|> Enum.sum

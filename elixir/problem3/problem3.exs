becda6ba28e803dfb717cca37b0df5343b28b2c1
Andre LaFleur <andre.lafleur@utah.edu>
2020-05-02 16:19:57 -0600

    Extract module to separate utility directory

--- elixir/problem3/problem3.exs
+++ elixir/problem3/problem3.exs
@@ -1,24 +1,3 @@
-defmodule Primality do
-	def is_prime(2), do: true
-	def is_prime(3), do: true
-	def is_prime(x) do
-		limit = round(:math.sqrt(x))
-		range = 2..limit
-		remainders_of(range, x)
-			|> Enum.count(fn remainder -> remainder === 0 end) === 0
-	end
-
-	def largest_prime_factor_of(value) do
-		limit = round(:math.sqrt(value))
-		range = 2..limit
-		Enum.filter(range, fn x -> rem(value, x) === 0 end)
-			|> Enum.filter(fn x -> is_prime(x) end)
-			|> Enum.max
-	end
-
-	defp remainders_of(range, dividend) do
-		Enum.map(range, fn potential_factor -> rem(dividend, potential_factor) end)
-	end
-end
+Code.compile_file("primality.exs", "../util")
 
 IO.inspect Primality.largest_prime_factor_of(600851475143)

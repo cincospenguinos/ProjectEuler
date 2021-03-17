25fa7869e9a676eb4db9ac9a9a46381751511354
Andre LaFleur <cincospenguinos@gmail.com>
2021-03-16 18:13:07 -0600

    A stupidly simple fix to ensure it goes a lot faster

--- go/problem3/problem3.go
+++ go/problem3/problem3.go
@@ -8,22 +8,26 @@ import (
 func main() {
 	number := 600851475143
 	largestPrimeFactor := 0
+	allPrimeFactors := make([]int, 0)
 
 	for toTest := 2; toTest < number; toTest += 1 {
 		isFactor := number % toTest == 0
 
 		if isFactor && isPrime(toTest) {
-			fmt.Println(toTest, "is largest factor")
 			largestPrimeFactor = toTest
+			allPrimeFactors = append(allPrimeFactors, largestPrimeFactor)
 
-			if number / largestPrimeFactor < largestPrimeFactor {
+			n := number
+			for _, factor := range allPrimeFactors {
+				n /= factor
+			}
+
+			if n == 1 {
 				fmt.Println(largestPrimeFactor)
 				return
 			}
 		}
 	}
-
-	fmt.Println(largestPrimeFactor)
 }
 
 func isPrime(number int) bool {
